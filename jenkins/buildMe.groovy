2f606287a761dba6a38093acf0892bb3164681aa
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2022-11-24 12:25:46 +0100

    Generate parameters

--- jenkins/buildMe.groovy
+++ jenkins/buildMe.groovy
@@ -1,11 +1,27 @@
 pipeline {
     agent any
     stages {
+         stage('Setup parameters') {
+                    steps {
+                        script { 
+                            properties([
+                                parameters([
+                                    choice(
+                                        choices: ['Release', 'Debug'], 
+                                        name: 'build_type',
+                                        description: 'Choose build type in cmake'
+                                    ),
+                                ])
+                            ])
+                        }
+                    }
+                }
+
         stage("build") {
             steps {
                 sh 'mkdir build; \
                 cd build; \
-                cmake ..; \
+                cmake -DCMAKE_BUILD_TYPE=${params.build_type} ..; \
                 make;'
             }
         }
